5097b24b75f5d7622f32b7390a5d8096592309fb
Marcela Rodriguez <arilaen@gmail.com>
2019-11-24 17:50:02 -0500

    update ImplicitBiasTestBlocks to use real number of test rounds

--- src/constants/ImplicitBiasTestBlocks.js
+++ src/constants/ImplicitBiasTestBlocks.js
@@ -2,32 +2,32 @@ import DISPLAY_TYPE from '../constants/TargetCategoryDisplayType';
 
 const IMPLICIT_BIAS_TEST_BLOCKS = [
   {
-    numTrials: 3,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.INCOMPATIBLE_TARGETS_ONLY
   },
   {
-    numTrials: 3,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.CATEGORIES_ONLY
   },
   {
-    numTrials: 3,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.INCOMPATIBLE_ALL
   },
   {
-    numTrials: 4,
+    numTrials: 40,
     displayType: DISPLAY_TYPE.INCOMPATIBLE_ALL,
     critical: true
   },
   {
-    numTrials: 3,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.COMPATIBLE_TARGETS_ONLY
   },
   {
-    numTrials: 3,
+    numTrials: 20,
     displayType: DISPLAY_TYPE.COMPATIBLE_ALL
   },
   {
-    numTrials: 4,
+    numTrials: 40,
     displayType: DISPLAY_TYPE.COMPATIBLE_ALL,
     critical: true
   }
